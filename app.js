7820185ba06f67da63815b6dfcf73723105f45c5
parthp2 <patelparth622@gmail.com>
2018-08-27 15:20:56 -0700

    encoded URI

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request=require('request');
 
+const yargs= require('yargs');
+
+const argv= yargs
+    .options({
+        a:{
+            describe:'give address you want to see weather',
+            demand:true,
+            alias:'address',
+            string:true
+        }
+    })
+    .help()
+    .argv;
+
+var encodeAddress= encodeURIComponent(argv.a);
+
 request({
-    url:"https://maps.googleapis.com/maps/api/geocode/json?address=1520%20new%20avenue%20san%20gabril",
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
     json:true
 },(error,response,body)=>{
     console.log(`Address:${body.results[0].formatted_address}`);
